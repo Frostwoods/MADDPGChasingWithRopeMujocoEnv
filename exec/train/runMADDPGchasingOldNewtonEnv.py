1e7cd61f41aa1ba2eb86d7f9251fbb37c1bbe2c0
ChenyaGu <me_chenya@126.com>
2021-06-23 14:57:41 +0800

    the final ver that performs well

--- exec/train/runMADDPGchasingOldNewtonEnv.py
+++ exec/train/runMADDPGchasingOldNewtonEnv.py
@@ -1,11 +1,13 @@
 import os
 import sys
+
 os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
-os.environ['KMP_DUPLICATE_LIB_OK']='True'
+os.environ['KMP_DUPLICATE_LIB_OK'] = 'True'
 dirName = os.path.dirname(__file__)
 sys.path.append(os.path.join(dirName, '..'))
 sys.path.append(os.path.join(dirName, '..', '..'))
 import logging
+
 logging.getLogger('tensorflow').setLevel(logging.ERROR)
 import numpy as np
 import json
@@ -15,22 +17,24 @@ import math
 
 from src.maddpg.trainer.myMADDPG import BuildMADDPGModels, TrainCritic, TrainActor, TrainCriticBySASR, \
     TrainActorFromSA, TrainMADDPGModelsWithBuffer, ActOneStep, actByPolicyTrainNoisy, actByPolicyTargetNoisyForNextState
-from src.RLframework.RLrun_MultiAgent import UpdateParameters, SampleOneStep, SampleFromMemory,\
+from src.RLframework.RLrun_MultiAgent import UpdateParameters, SampleOneStep, SampleFromMemory, \
     RunTimeStep, RunEpisode, RunAlgorithm, getBuffer, SaveModel, StartLearn
 from src.functionTools.loadSaveModel import saveVariables
 # from env.multiAgentEnv import GetActionCost,RewardSheep, RewardWolf, Observe, IsCollision, getPosFromAgentState, getVelFromAgentState,PunishForOutOfBound,ReshapeAction, TransitionFunctionWithoutXPos, ResetMultiAgentNewtonChasing
-from env.multiAgentEnv import GetActionCost,RewardSheep, RewardWolf, Observe, IsCollision, getPosFromAgentState, getVelFromAgentState,PunishForOutOfBound, StayInBoundaryByReflectVelocity,ResetMultiAgentNewtonChasing,TransitMultiAgentChasingForExp, ReshapeAction, GetCollisionForce, ApplyActionForce, ApplyEnvironForce, IntegrateState, getPosFromAgentState,getVelFromAgentState,ResetMultiAgentChasingWithVariousSheep
-from src.functionTools.editEnvXml import transferNumberListToStr,MakePropertyList,changeJointProperty
-
+from env.multiAgentEnv import GetActionCost, RewardSheep, RewardWolf, Observe, IsCollision, getPosFromAgentState, \
+    getVelFromAgentState, PunishForOutOfBound, StayInBoundaryByReflectVelocity, ResetMultiAgentNewtonChasing, \
+    TransitMultiAgentChasingForExp, ReshapeAction, GetCollisionForce, ApplyActionForce, ApplyEnvironForce, \
+    IntegrateState, getPosFromAgentState, getVelFromAgentState, ResetMultiAgentChasingWithVariousSheep
+from src.functionTools.editEnvXml import transferNumberListToStr, MakePropertyList, changeJointProperty
 
 # fixed training parameter,
 maxEpisode = 120000
-learningRateActor = 0.01#
-learningRateCritic = 0.01#
-gamma = 0.95 #
-tau=0.01 #
-bufferSize = 1e6#
-minibatchSize = 1024#
+learningRateActor = 0.01  #
+learningRateCritic = 0.01  #
+gamma = 0.95  #
+tau = 0.01  #
+bufferSize = 1e6  #
+minibatchSize = 1024  #
 
 
 # 7.13 add action cost
@@ -58,24 +62,27 @@ def main():
         numSheeps = int(condition['numSheeps'])
         numBlocks = 0
 
-        maxTimeStep = 75# int(condition['maxTimeStep'])
-        sheepSpeedMultiplier =1 #float(condition['sheepSpeedMultiplier'])
-        individualRewardWolf =1 #float(condition['individualRewardWolf'])
-        costActionRatio = 0#float(condition['costActionRatio'])
+        maxTimeStep = 75  # int(condition['maxTimeStep'])
+        sheepSpeedMultiplier = 1  # float(condition['sheepSpeedMultiplier'])
+        individualRewardWolf = 1  # float(condition['individualRewardWolf'])
+        costActionRatio = 0  # float(condition['costActionRatio'])
 
         saveAllmodels = 1
 
-    print("maddpg: {} wolves, {} sheep, {} blocks, {} episodes with {} steps each eps, sheepSpeed: {}x, wolfIndividualReward: {}, save all models: {}".
-          format(numWolves, numSheeps, numBlocks, maxEpisode, maxTimeStep, sheepSpeedMultiplier, individualRewardWolf, str(saveAllmodels)))
+    print(
+        "maddpg: {} wolves, {} sheep, {} blocks, {} episodes with {} steps each eps, sheepSpeed: {}x, wolfIndividualReward: {}, save all models: {}".
+        format(numWolves, numSheeps, numBlocks, maxEpisode, maxTimeStep, sheepSpeedMultiplier, individualRewardWolf,
+               str(saveAllmodels)))
 
-
-    dataFolder = os.path.join(dirName, '..','..', 'data')
-    mainModelFolder = os.path.join(dataFolder,'model')
-    modelFolder = os.path.join(mainModelFolder, 'originEnv6.14','indvidulReward={}'.format(individualRewardWolf),'{} wolves, {} sheep, {} blocks'.format(numWolves, numSheeps, numBlocks))
+    dataFolder = os.path.join(dirName, '..', '..', 'data')
+    mainModelFolder = os.path.join(dataFolder, 'model')
+    modelFolder = os.path.join(mainModelFolder, 'originEnv0618', 'indvidulReward={}'.format(individualRewardWolf),
+                               '{} wolves, {} sheep, {} blocks'.format(numWolves, numSheeps, numBlocks))
 
     if not os.path.exists(modelFolder):
         os.makedirs(modelFolder)
 
+    # -------agent size & speed setting------
     numAgents = numWolves + numSheeps
     numEntities = numAgents + numBlocks
     wolvesID = list(range(numWolves))
@@ -87,7 +94,7 @@ def main():
     blockSize = 0.2
     entitiesSizeList = [wolfSize] * numWolves + [sheepSize] * numSheeps + [blockSize] * numBlocks
 
-    wolfMaxSpeed = 1
+    wolfMaxSpeed = 1.0
     blockMaxSpeed = None
     sheepMaxSpeedOriginal = 1.3
     sheepMaxSpeed = sheepMaxSpeedOriginal * sheepSpeedMultiplier
@@ -96,10 +103,12 @@ def main():
     entitiesMovableList = [True] * numAgents + [False] * numBlocks
     massList = [1.0] * numEntities
 
-    collisionReward = 10 # originalPaper = 10*3
+    collisionReward = 10  # originalPaper = 10*3
     isCollision = IsCollision(getPosFromAgentState)
     punishForOutOfBound = PunishForOutOfBound()
-    rewardSheep = RewardSheep(wolvesID, sheepsID, entitiesSizeList, getPosFromAgentState, isCollision,punishForOutOfBound, collisionPunishment = collisionReward) # TODO collisionReward = collisionPunishment
+    rewardSheep = RewardSheep(wolvesID, sheepsID, entitiesSizeList, getPosFromAgentState, isCollision,
+                              punishForOutOfBound,
+                              collisionPunishment=collisionReward)  # TODO collisionReward = collisionPunishment
 
     rewardWolf = RewardWolf(wolvesID, sheepsID, entitiesSizeList, isCollision, collisionReward, individualRewardWolf)
     reshapeAction = ReshapeAction()
@@ -110,32 +119,35 @@ def main():
     # rewardWolf = RewardWolf(wolvesID, sheepsID, entitiesSizeList, isCollision)
     rewardFunc = lambda state, action, nextState: \
         list(rewardWolf(state, action, nextState)) + list(rewardSheep(state, action, nextState))
-    
-    minDistanceForReborn = 10
-    # numPlayers = 2
-    gridSize = 60
+
+    # minDistanceForReborn = 10
+    # gridSize = 60
     # reset0 = ResetMultiAgentNewtonChasing(gridSize, numWolves, minDistanceForReborn)
-    reset0 =  ResetMultiAgentChasingWithVariousSheep(numWolves, numBlocks)
-    reset = lambda :reset0(numSheeps)
-    # reset = ResetMultiAgentChasing(numAgents, numBlocks)
-    observeOneAgent = lambda agentID: Observe(agentID, wolvesID, sheepsID, blocksID, getPosFromAgentState, getVelFromAgentState)
+    reset0 = ResetMultiAgentChasingWithVariousSheep(numWolves, numBlocks)
+    reset = lambda: reset0(numSheeps)
+    observeOneAgent = lambda agentID: Observe(agentID, wolvesID, sheepsID, blocksID, getPosFromAgentState,
+                                              getVelFromAgentState)
     observe = lambda state: [observeOneAgent(agentID)(state) for agentID in range(numAgents)]
-    
+
     # stayInBoundaryByReflectVelocity = StayInBoundaryByReflectVelocity( [0, gridSize - 1], [0, gridSize - 1])
-    stayInBoundaryByReflectVelocity = StayInBoundaryByReflectVelocity( [-1,1], [-1, 1])
+    stayInBoundaryByReflectVelocity = StayInBoundaryByReflectVelocity([-1, 1], [-1, 1])
+
     def checkBoudary(agentState):
-        newState = stayInBoundaryByReflectVelocity(getPosFromAgentState(agentState),getVelFromAgentState(agentState))
+        newState = stayInBoundaryByReflectVelocity(getPosFromAgentState(agentState), getVelFromAgentState(agentState))
         return newState
-    checkAllAgents = lambda states:[checkBoudary(agentState) for agentState in states]
+
+    checkAllAgents = lambda states: [checkBoudary(agentState) for agentState in states]
     reshapeAction = ReshapeAction()
     getCollisionForce = GetCollisionForce()
     applyActionForce = ApplyActionForce(wolvesID, sheepsID, entitiesMovableList)
-    applyEnvironForce = ApplyEnvironForce\
-        (numEntities, entitiesMovableList, entitiesSizeList,  getCollisionForce, getPosFromAgentState)
-    integrateState = IntegrateState(numEntities, entitiesMovableList, massList, entityMaxSpeedList, getVelFromAgentState, getPosFromAgentState)
-    transit = TransitMultiAgentChasingForExp(reshapeAction, applyActionForce, applyEnvironForce, integrateState,checkAllAgents)
+    applyEnvironForce = ApplyEnvironForce \
+        (numEntities, entitiesMovableList, entitiesSizeList, getCollisionForce, getPosFromAgentState)
+    integrateState = IntegrateState(numEntities, entitiesMovableList, massList, entityMaxSpeedList,
+                                    getVelFromAgentState, getPosFromAgentState)
+    transit = TransitMultiAgentChasingForExp(reshapeAction, applyActionForce, applyEnvironForce, integrateState,
+                                             checkAllAgents)
 
-    isTerminal = lambda state: [False]* numAgents
+    isTerminal = lambda state: [False] * numAgents
     initObsForParams = observe(reset())
     obsShape = [initObsForParams[obsID].shape[0] for obsID in range(len(initObsForParams))]
 
@@ -144,7 +156,7 @@ def main():
 
     layerWidth = [128, 128]
 
-#------------ models ------------------------
+    # ------------ models ------------------------
 
     buildMADDPGModels = BuildMADDPGModels(actionDim, numAgents, obsShape)
     modelsList = [buildMADDPGModels(layerWidth, agentID) for agentID in range(numAgents)]
@@ -154,7 +166,7 @@ def main():
     trainActorFromSA = TrainActorFromSA(learningRateActor)
     trainActor = TrainActor(trainActorFromSA)
 
-    paramUpdateInterval = 1 #
+    paramUpdateInterval = 1  #
     updateParameters = UpdateParameters(paramUpdateInterval, tau)
     sampleBatchFromMemory = SampleFromMemory(minibatchSize)
 
@@ -162,13 +174,14 @@ def main():
     learningStartBufferSize = minibatchSize * maxTimeStep
     startLearn = StartLearn(learningStartBufferSize, learnInterval)
 
-    trainMADDPGModels = TrainMADDPGModelsWithBuffer(updateParameters, trainActor, trainCritic, sampleBatchFromMemory, startLearn, modelsList)
+    trainMADDPGModels = TrainMADDPGModelsWithBuffer(updateParameters, trainActor, trainCritic, sampleBatchFromMemory,
+                                                    startLearn, modelsList)
 
     actOneStepOneModel = ActOneStep(actByPolicyTrainNoisy)
     actOneStep = lambda allAgentsStates, runTime: [actOneStepOneModel(model, allAgentsStates) for model in modelsList]
 
     sampleOneStep = SampleOneStep(transit, rewardFunc)
-    runTimeStep = RunTimeStep(actOneStep, sampleOneStep, trainMADDPGModels, observe = observe)
+    runTimeStep = RunTimeStep(actOneStep, sampleOneStep, trainMADDPGModels, observe=observe)
 
     runEpisode = RunEpisode(reset, runTimeStep, maxTimeStep, isTerminal)
 
@@ -176,11 +189,12 @@ def main():
     getModelList = [getAgentModel(i) for i in range(numAgents)]
     modelSaveRate = 2000
     fileName = "maddpg{}wolves{}sheep{}blocks{}episodes{}individ{}_agent".format(
-        numWolves, numSheeps, numBlocks, maxEpisode, maxTimeStep,   individualRewardWolf)
+        numWolves, numSheeps, numBlocks, maxEpisode, maxTimeStep, individualRewardWolf)
 
     # folderName = 'maddpg_10reward_full'
     modelPath = os.path.join(modelFolder, fileName)
-    saveModels = [SaveModel(modelSaveRate, saveVariables, getTrainedModel, modelPath + str(i), saveAllmodels) for i, getTrainedModel in enumerate(getModelList)]
+    saveModels = [SaveModel(modelSaveRate, saveVariables, getTrainedModel, modelPath + str(i), saveAllmodels) for
+                  i, getTrainedModel in enumerate(getModelList)]
 
     maddpg = RunAlgorithm(runEpisode, maxEpisode, saveModels, numAgents)
     replayBuffer = getBuffer(bufferSize)
@@ -189,6 +203,3 @@ def main():
 
 if __name__ == '__main__':
     main()
-
-
-
