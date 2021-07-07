8b5747f01288b851c0b6a76db560336a6c440e93
coreknowledge <coreknowledge666@gmail.com>
2021-07-07 20:11:32 +0800

    commit for pull

--- exec/evluateRopePara/parallelRunMADDPGchasingMujocoEnvWithRopes.py
+++ exec/evluateRopePara/parallelRunMADDPGchasingMujocoEnvWithRopes.py
@@ -53,9 +53,9 @@ def main():
     manipulatedVariables['damping'] = [0.5]
     manipulatedVariables['frictionloss'] = [1.0]
     manipulatedVariables['masterForce'] = [1.0]
-    manipulatedVariables['killZone'] = [2.0, 4.0]
-    manipulatedVariables['ropePunishWeight'] = [0.3, 0.5]
-    manipulatedVariables['ropeLength'] = [0.06] #ssr-1,Xp = 0.06; ssr-3 =0.09
+    manipulatedVariables['killZone'] = [4.0]
+    manipulatedVariables['ropePunishWeight'] = [0.5]
+    manipulatedVariables['ropeLength'] = [0.09] #ssr-1,Xp = 0.06; ssr-3 =0.09
     manipulatedVariables['masterMass'] = [1.0] #ssr-1, ssr-3 = 1.0; Xp = 2.0
 
     productedValues = it.product(*[[(key, value) for value in values] for key, values in manipulatedVariables.items()])
